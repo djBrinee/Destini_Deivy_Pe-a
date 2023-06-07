6972b2b73ab27a095fa5e4aeefcf81fe9dd4fde0
djBrinee <deivyjunior12@gmail.com>
2023-06-06 22:19:52 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+//Done: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+//Done: Step 9 - Create a new storyBrain object from the StoryBrain class.
+final storyBrain = new StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        //Done: Step 1 - Add background.png to this Container as a background image.
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('images/background.png'), fit: BoxFit.cover),
+        ),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    //Done: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    //Done: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    //Done: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+                    // What´s messing is the SetState function
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),
+                  child: Text(
+                    //Done: Step 13 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.getChoice1(),
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //Done: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //Done: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //Done: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+                    child: Text(
+                      //Done: Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
